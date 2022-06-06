090d486d150f46403d182e4ff4345f167f4d4df4
LENOVO <LENOVO@192.168.0.5>
2022-06-06 20:12:05 +0530

    fresh

---
+++ NewProject/src/main/java/pom/Login.java
@@ -0,0 +1,17 @@
+package pom;
+
+public class Login {
+	static int a = 10;
+	static int b = 20;
+	public static void test() {
+		int c = a+b;
+		System.out.println(c);
+		
+	}
+
+	public static void main(String[] args) {
+		Login.test();
+
+	}
+
+}
