18468f38c4b728911742bc7ab6a36367a363d959
LENOVO <LENOVO@192.168.0.5>
2022-06-06 20:29:55 +0530

    fresh1

--- NewProject/src/main/java/pom/Home.java
+++ NewProject/src/main/java/pom/Home.java
@@ -3,5 +3,7 @@ package pom;
 public class Home {
 public static void main(String[] args) {
 	System.out.println("my githhub is working now");
+	
+	System.out.println("we have created new master file");
 }
 }
